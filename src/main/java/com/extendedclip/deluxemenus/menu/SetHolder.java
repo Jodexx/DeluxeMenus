88925e03b3c5740d0f6164d56eee6b32ca6af44c
Jodexx <0984134q@gmail.com>
2024-07-15 23:51:33 +0300

    add SetHolder

---
+++ src/main/java/com/extendedclip/deluxemenus/menu/SetHolder.java
@@ -0,0 +1,10 @@
+package com.extendedclip.deluxemenus.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetHolder {
+    public List<Integer> material = new ArrayList<>();
+    public List<Integer> name = new ArrayList<>();
+    public List<Integer> lore = new ArrayList<>();
+}
